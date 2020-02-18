b74d8844904579511884d960a33bfd8d86554c52
seamo88 <bstockgame.88@gmail.com>
2020-02-18 11:26:00 -0600

    Add project files.

---
+++ 2_4_Lab_SeanM/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _2_4_Lab_SeanM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int myFirstNumber = 19;
+            int mySecondNumber = 244;
+            Console.WriteLine(myFirstNumber * mySecondNumber);
+
+            string myText = "Some Text";
+                Console.WriteLine(myText + mySecondNumber);
+
+            string moreText = "Goes Here";
+            Console.WriteLine(myText +" "+ moreText);
+
+            string color1 = "Yellow";
+            string color2 = "Red";
+
+            bool isYellow = true;
+            bool isGreen = false;
+
+            Console.WriteLine(color1 + isYellow + color2 + isGreen);
+
+            double numbers;
+
+            numbers = mySecondNumber / (double)myFirstNumber;
+
+            Console.WriteLine(numbers);
+
+            Console.WriteLine("Breakpoint Here");
+
+            string textToNumber = "80473";
+            int results = 99 * int.Parse(textToNumber);
+            Console.WriteLine(results);
+
+            DateTime rightNow = DateTime.Now;
+            Console.WriteLine(rightNow);
+        }
+    }
+}
